d961da62fe7838875b4289a7ead73669abe2db97
sai250 <dsaibhargav567@gmail.com>
2020-10-30 11:02:19 +0530

    Updates

---
+++ precache-manifest.764a0838b40fd17809ed4b735daa04ef.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962c572dbae1170b864756a04c56220e",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/react-website/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "7a0bf261a514030c19d6",
+    "url": "/react-website/static/css/main.7a815947.chunk.css"
+  },
+  {
+    "revision": "2ac5e7ffc73930e26b60",
+    "url": "/react-website/static/js/2.670fdb07.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/react-website/static/js/2.670fdb07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a0bf261a514030c19d6",
+    "url": "/react-website/static/js/main.ce77305f.chunk.js"
+  },
+  {
+    "revision": "519ad5c5e1c350c1a7dd",
+    "url": "/react-website/static/js/runtime-main.4c2e43f8.js"
+  },
+  {
+    "revision": "19a2e0eab24e9e902408d4f8547eeab9",
+    "url": "/react-website/static/media/android.19a2e0ea.jpeg"
+  },
+  {
+    "revision": "70cea92af61399debee3717652a63569",
+    "url": "/react-website/static/media/app.70cea92a.jpeg"
+  },
+  {
+    "revision": "ac8f6f9ad22a8902b83e21560b95dca5",
+    "url": "/react-website/static/media/digital.ac8f6f9a.jpeg"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/react-website/static/media/hero-img.619f0cc8.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/react-website/static/media/img2.e981619f.svg"
+  },
+  {
+    "revision": "2eed95a2d555c404f1f451730eaa9eef",
+    "url": "/react-website/static/media/marketing.2eed95a2.jpg"
+  },
+  {
+    "revision": "1192a86d7075a15ba491f01cebceca0a",
+    "url": "/react-website/static/media/s1.1192a86d.jpg"
+  },
+  {
+    "revision": "43049cae060f2a2a2acb77007f8482f9",
+    "url": "/react-website/static/media/software.43049cae.jpeg"
+  }
+]);
